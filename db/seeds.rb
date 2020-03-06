da469b32e5c68107d215259c6d07faf0a9645146
eve jourdan <eve.jourdan@gmail.com>
2020-03-06 14:59:28 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cocktails deleting"
+puts "Ingredients deleting"
+puts "Doses deleting"
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+puts "Cocktails deleted"
+puts "Ingredients deleted"
+puts "Doses deleted"
+
+
+puts "Ingredient creating"
 Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
 Ingredient.create(name: 'ice cube')
 Ingredient.create(name: 'strawberry')
+puts "Ingredient created"
 
+puts "Dose creating"
 dose1 = Dose.create(description: "a", cocktail_id: 1, ingredient_id: 1)
 puts "created #{Dose.count}"
+
