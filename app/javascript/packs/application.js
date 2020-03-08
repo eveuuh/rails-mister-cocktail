a29c128274f7d7b00b48f88f9ec2ca7d95fe51e6
eve jourdan <eve.jourdan@gmail.com>
2020-03-08 13:55:16 +0100

    precompile assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,6 @@
 
 console.log('Hello World from Webpacker')
 import 'bootstrap';
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-initUpdateNavbarOnScroll();
+// import { initUpdateNavbarOnScroll } from '../components/navbar';
+// initUpdateNavbarOnScroll();
 
